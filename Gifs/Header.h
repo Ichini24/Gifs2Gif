a67e08e358479b2117410befcf4edc73af9bf7b9
Ichini24 <ichini24666@gmail.com>
2018-08-16 23:59:20 +0300

    Add project files.

---
+++ Gifs/Header.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include "gif.h"
+double tt_tic = 0;
+
+void tic() {
+	tt_tic = cv::getTickCount();
+}
+void toc() {
+	double tt_toc = (cv::getTickCount() - tt_tic) / (cv::getTickFrequency());
+	printf("toc: %4.3f sn\n", tt_toc);
+}
---
+++ Gifs/Source.cpp
@@ -0,0 +1,34 @@
+#include "Header.h"
+
+int main()
+{
+	std::string path = "D:\\giffs\\new\\";
+	cv::VideoCapture tc(path + std::to_string(0) + ".gif");
+	cv::Mat frame;
+	tc >> frame;
+
+	GifWriter writer;
+	
+	uint32_t ww = frame.cols, wh = frame.rows;
+	tic();
+	GifBegin(&writer, "D:\\giffs\\new\\output.gif", ww, wh, 3);
+	for (int i = 0; i < 6; i++)
+	{
+		cv::VideoCapture cap(path + std::to_string(i) + ".gif");
+		for (; ; )
+		{
+			cap >> frame;
+			if (frame.empty())
+			{
+				break;
+			}
+			cv::cvtColor(frame, frame, cv::COLOR_BGR2RGBA);
+			GifWriteFrame(&writer, frame.data, ww, wh, 3);
+		}
+	}
+	GifEnd(&writer);
+	toc();
+
+
+	return 1;
+}
